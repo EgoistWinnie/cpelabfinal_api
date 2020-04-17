cace626856b7a50b13a3b7c4c85105cdfc3c0d51
winnieinyourarea <thepiggywin2000@gmail.com>
2020-04-17 19:15:53 +0700

    for heroku

--- Server.js
+++ Server.js
@@ -8,13 +8,22 @@ const app = express()
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended: true}))
 // mongodb+srv://therealwinnie:<password>@cluster0-zfzar.mongodb.net/test?retryWrites=true&w=majority
+// Enable CORS
+app.use(function (req, res, next) {
+    //res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 
 const Router = require('./Route')
 app.use(Router)
 
 const PORT = process.env.PORT || 5000
-const HOSTNAME = process.env.HOSTNAME
+const HOSTNAME = process.env.HOSTNAME || "localhost"
 app.listen(PORT,HOSTNAME, () => {
     console.log('Server is listening at:'+HOSTNAME+':'+PORT)
 })
